c2a31144d272f51b8571fc5e72a0452e64ddf4cc
Your Name <dan.weibren@gmail.com>
2026-02-01 18:23:16 +0200

    feat(routes): define core feature paths and default redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { IngredientLedgerComponent } from './components/ingredient-ledger/ingredient-ledger.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { DishesComponent } from './components/dishes/dishes.component';
+import { MenuCreatingComponent } from './components/menu-creating/menu-creating.component';
+import { ChecklistCreatorComponent } from './components/checklist-creator/checklist-creator.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeScreenComponent },
-  { path: 'ingredients', component: IngredientLedgerComponent }
+  { path: 'ingredients', component: IngredientLedgerComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'dishes', component: DishesComponent },
+  { path: 'menu-creating', component: MenuCreatingComponent },
+  { path: 'checklist-creator', component: ChecklistCreatorComponent },
+  { path: '', redirectTo: 'ingredients', pathMatch: 'full' }, // Default route
 ];
