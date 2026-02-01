7cc5bb63701c7e6179ce8f1ea9bd7bedec28d2bd
Your Name <dan.weibren@gmail.com>
2026-02-01 18:14:45 +0200

    feat(home-screen): implement initial home screen and refactor ingredient-ledger component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeScreenComponent } from './components/home-screen/home-screen.component';
+import { IngredientLedgerComponent } from './components/ingredient-ledger/ingredient-ledger.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeScreenComponent },
+  { path: 'ingredients', component: IngredientLedgerComponent }
+];
---
+++ src/app/components/home-screen/home-screen.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-screen',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './home-screen.component.html',
+  styleUrl: './home-screen.component.scss'
+})
+export class HomeScreenComponent {
+  title = 'foodVibe';
+}
