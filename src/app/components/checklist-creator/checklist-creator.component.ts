4086d7811beb5a16f492745e145e02840fdc5cf9
Your Name <dan.weibren@gmail.com>
2026-02-01 18:23:18 +0200

    feat(components): scaffold feature placeholder components

---
+++ src/app/components/checklist-creator/checklist-creator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checklist-creator',
+  standalone: true,
+  imports: [],
+  templateUrl: './checklist-creator.component.html',
+  styleUrl: './checklist-creator.component.scss'
+})
+export class ChecklistCreatorComponent {
+
+}
---
+++ src/app/components/menu-creating/menu-creating.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-creating',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-creating.component.html',
+  styleUrl: './menu-creating.component.scss'
+})
+export class MenuCreatingComponent {
+
+}
