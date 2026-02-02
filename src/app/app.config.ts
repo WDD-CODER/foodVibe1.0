dae91aed9ba57628389934092dd7839433d3eafe
Your Name <dan.weibren@gmail.com>
2026-02-02 10:54:32 +0200

    refactor(core): update ItemDataService provider in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { IngredientDataService } from './core/services/ingredient-data.service';
+import { ItemDataService } from './core/services/ingredient-data.service';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), IngredientDataService]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), ItemDataService]
 };
