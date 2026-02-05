9f15aeda3d473365274a5c99e84be0e495923998
Your Name <dan.weibren@gmail.com>
2026-02-05 22:45:55 +0200

    feat(stack): register ArrowLeft and Edit icons in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { ItemDataService } from './core/services/items-data.service';
 
 import { routes } from './app.routes';
+import { ArrowLeft, Edit, LucideAngularModule, Plus, Trash2 } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), ItemDataService]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes), 
+    ItemDataService,
+    importProvidersFrom(
+      LucideAngularModule.pick({ Plus, Trash2, Edit, ArrowLeft }) // Register 'Plus' here
+    )
+  ]
 };
